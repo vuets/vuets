19916c5e3829bd6c3d24d256d16dcc1b02c030ce
dyu <david.yu.ftw@gmail.com>
2016-10-01 16:02:01 +0800

    set -e

--- release.sh
+++ release.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
+set -e
+
 VERSION=$1
 
-[ ! -n "$VERSION" ] && set -e && echo "Enter release version: " && read VERSION
+[ ! -n "$VERSION" ] && echo "Enter release version: " && read VERSION
 
-echo "Releasing $VERSION - are you sure? (y/n):" && read CONFIRM
+echo "Releasing $VERSION - are you sure? (y/n):" && read CONFIRM && [ "$CONFIRM" != "y" ] && exit 0
 
-if [ "$CONFIRM" = "y" ]; then
-  npm run build
-  # commit
-  git add -A
-  git commit -m "[build] $VERSION"
-  npm version $VERSION --message "[release] $VERSION"
+npm run build
 
-  # publish
-  git push origin refs/tags/v$VERSION
-  git push
-  npm publish
-fi
+# commit
+git add -A
+git commit -m "[build] $VERSION"
+npm version $VERSION --message "[release] $VERSION"
+
+# publish
+git push origin refs/tags/v$VERSION
+git push
+npm publish
